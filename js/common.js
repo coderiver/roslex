e1c6469d907a7d6946463931c55c43264d39bb9c
andreyka_g <andreyka_g@ukr.net>
2015-04-02 01:36:52 +0300

    timers add

--- js/common.js
+++ js/common.js
@@ -18,4 +18,13 @@ head.ready(function() {
 	  variableWidth: true,
    arrows: false,
 });
+$('.form__timer').timeTo({
+    timeTo: new Date(new Date('Thu Apr 09 2015 09:00:00 GMT+0300 (Финляндия (лето))')),
+    displayDays: 2,
+    // theme: "w",
+    displayCaptions: true,
+    fontSize: 35,
+    captionSize: 14,
+    lang: 'ru'
+});
 });
